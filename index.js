f5db84fe34979794d109532595754f894b42ad43
LorkiNikita <69603120+LorkiNikita@users.noreply.github.com>
2021-08-21 20:36:55 +0300

    Commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const message = "Я брошу клики сам";
 const button = document.querySelector("button");
 const input = document.querySelector(".password");
-const text = document.querySelector(".text") ;
+const text_ = document.querySelector(".text") ;
 
 let clicks = 0;
 input.oninput = function () {
@@ -10,9 +10,13 @@ input.oninput = function () {
 button.addEventListener("click" , () => {
   if (input.value == message ) {
     if (clicks > 7) {
-      
+      text_.textContent = "Молодец.Просто отключи расширение Firefox SPEED NC";
+      clicks = 0;
+      setTimeout(() => {
+        text_.textContent = "Текст подсказки";
+      }, 3000);
     } else {
-      
+      text_.textContent = "Так сложно вводить буквами?";
     }
   }
 });
